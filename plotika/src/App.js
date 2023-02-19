8f04d653fc17eb1ecefda3d57bcadddcb66c914c
Guido Santiago Reta <guidoreta@hotmail.com>
2023-02-19 12:10:31 -0300

    se agregan componentes: header-section y search-bar en la rama feat/start

--- plotika/src/App.js
+++ plotika/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import HeaderSection from './components/header-section/HeaderSection';
 import NavBar from './components/navbar/NavBar';
+import SearchBar from './components/search-bar/SearchBar';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <HeaderSection/>
+     <SearchBar/>
     </div>
   );
 }
