6ce54a6a7943489a95f964ef26f5792366b5e28e
Guido Santiago Reta <guidoreta@hotmail.com>
2023-02-19 23:30:57 -0300

    se agregar el footer a feature

---
+++ plotika/src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <>
+    <footer>
+     <div className="container-footer">
+       
+      <h2 className="last-title">
+        {" "}
+        Developed  by Guido Santiago Reta| SwareGrap &copy; 2023
+      </h2>
+    </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
